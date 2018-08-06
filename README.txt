0c66b8e8519bf1a3cf8e4545ad8d69f523dba0ef
sadrunslive <danteslimbo@gmail.com>
2018-08-06 02:07:26 -0700

    First major release
    Bug fixes, general usability, general functions implimented.

--- README.txt
+++ README.txt
@@ -1,7 +1,7 @@
 Follow me @Sadrunslive
 Add the WoW_Speedrun_Timer folder to your Addons folder.
 To join in further, join us at the WoW Speedrun Discord: https://discord.gg/KMkpFVt
-Version: 0.1
+Version: 1.0
 
 Currently a leveling timer has been implemented. More information will be added here as it is devolped.
 
@@ -9,54 +9,19 @@ Currently no translations are available. They will be introduced moving forward.
 
 The available slash commands:
 
-/timerGoal (goal level)
+/srGoal (goal level)
 Will display the current goal level, if you wish to change the level goal, you can set that by adding the desired level (Ex: "/timeGoal 20" would set my goal level to 20).
 
-/timerReady
+/srReady
 Will begin loading the timer, this MUST be done before runs if they wish to be recognized.
 
-/timerStop
+/srStop
 Will stop the timer.
 
-/timerCurrentTime
+/srSplit
 Displays the current "split", and where you are at in the run.
 
-Known issues:
-Entering instances will destory the timer. Possible fix is to move time collection to global variables that persist through load.
-You are able to change the goal level mid run. Simple fix, just not implemented in this pull.
-
 Current Goals and Status:
-Time from start to goal level (Complete)
-	Determine current level (Complete)
-	Determine when levels are achieved (Complete)
-	Store and calculate elapsed time (Complete)
-	Implement error catch system (Complete)
-Time completion of dungeon clearance (0%)
-	Determine when run begins (Complete)
-	Determine when dungeon completes (0%)
-	Determine when certain bosses are killed (splits) (0%)
-Time completion of raid clearance (0%)
-	Determine when run begins (Complete)
-	Determine when dungeon completes (0%)
-	Determine when certain bosses are killed (splits) (0%)
-Time completion of Death Knight starting zone (0%)
-	Determine when the zone has completed per rules on Speedrun.com/wow (0%)
-Time completion of Worgen starting zone (0%)
-	Determine when the zone has completed per rules on Speedrun.com/wow (0%)
-Time completion of Goblin starting zone (0%)
-	Determine when the zone has completed per rules on Speedrun.com/wow (0%)
-Time completion of dungeon sets (0%)
-	Determine when run begins (Complete)
-	Determine when dungeon completes (0%)
-	Determine when certain bosses are killed (splits) (0%)
-	Determine when certain dungeons are complete in the set (splits) (0%)
-Time completion of raid sets (0%)
-	Determine when run begins (Complete)
-	Determine when dungeon completes (0%)
-	Determine when certain bosses are killed (splits) (0%)
-	Determine when certain dungeons are complete in the set (splits) (0%)
-Time completion of Chromie Scenario (0%)
-	Determine when the instance is finished (0%)
 Add translations for all displayed text
 	English (Complete)
 	French (0%)
--- WoW_Speedrun_Timer/WoW_Speedrun_Timer.lua
+++ WoW_Speedrun_Timer/WoW_Speedrun_Timer.lua
@@ -1,12 +1,12 @@
---local startTimeIGT = GetSessionTime(); --Initializes the In Game Timer (IGT) start to the current session timer (Might change to 0)
---local startTimeRT = GetSessionTime(); --Initializes the Real Time (RT) start time to the current session timer (Might change to 0)
 local endTime = 0; --Initializes the end time variable to 0
 
 local accumTimeIGT = 0; --Initializes the accumulating time variable that is used when the player enters a loading screen. Should not include reloads and moving into instances.
---local accumTimeRT = 0; --Initializes a back up variable in case a real time one is needed, see above variable
 
---local timerRunning = false; --Initializes the boolean variable to determine if the timmer is currently running or not
---local playerLoaded = false; --Stores if the character has loaded into the game for the first time
+local currentSplit = 0; --Initializes the variable that will hold the current split timing
+
+local runStatus = false; --Initializes the run as not currently being started. This allows for runs to calculate through load zones.
+local timerRunning = false; --Initializes the boolean variable to determine if the timmer is currently running or not
+local playerLoaded = false; --Stores if the character has loaded into the game for the first time
 
 local goalLevel = 10; --Initializes the variable that will hold the goal level. (Should be made into carryover var?)
 
@@ -20,49 +20,31 @@ local WOW_HIGHEST_LEVEL = 110; --Initializes the highest level attainable by the
 	----------------------------
 	--EVENT THAT WILL DETERMINE ENDING TIME--
 	function events:UNIT_LEVEL(playerLevel, ...) --event will fire when the player unit levels (args: "player")
-		if playerLevel == "player" then
-			if UnitLevel("player") == tonumber(goalLevel) then --The number here decides at which level the run will stop. Must be a number between 2-110 inclusive
+		if playerLevel == "player" then --if the player level function is called
+			if UnitLevel("player") >= tonumber(goalLevel) then --The number here decides at which level the run will stop. Must be a number between 2-110 inclusive
 				endTime = GetSessionTime(); --Gets the current time the level is acieved and stores it in the end time IGT variable.
 			
 			
-				print("Congratulations! Your time:")
+				print("Congratulations! Your time:") --print a congratulations message
+				currentSplit = (difftime(endTime, startTimeIGT) + accumTimeIGT) --set currentSplit to the current split time
+				calculateInGameTime(currentSplit, ...) --send the currentSplit time to the function to calculate in game time
 			
-				calculateRealTime(endTime, startTimeRT, ...);
-				calculateInGameTime(endTime, startTimeIGT, ...);
-			
-				timerRunning = false;
-				startTimeIGT = GetSessionTime();
-				startTimeRT = GetSessionTime();
-				
+				timerRunning = false; --since the speedrun is over, set timerRunning to false
+				startTimeIGT = GetSessionTime(); --the newest start time is refreshed to the current session time
 				playerLoaded = false; --resets the loaded state so the player can begin another run
 			
-			else
-				print("Current split for level", UnitLevel("player"), "of", goalLevel)
-				SlashCmdList.SRSPLIT()
+			elseif UnitLevel("player") < tonumber(goalLevel) then --As long as the level is less than the goal level, print out the splits
+				print("Current split for level", UnitLevel("player"), "of", goalLevel) --sends a message telling the user the current split
+				SlashCmdList.SRSPLIT() --runs the split function
 			end
 		end
 	end
 		
-	-- when a player goes to enter an instance
-		-- trigger the loading screen event
-			-- when this event triggers, grab the current split time in seconds, store to a holder variable
-		
-		-- trigger the loading screen ending event
-			-- when this event trigger, restart the current sessiontimer, but add the accum split time to checks.
-	
-	
-	
-	function events:LOADING_SCREEN_ENABLED(...) --should fire when the loading screen is enabled
-		--accumTimeIGT = difftime(endTime, startTimeIGT) + accumTimeIGT
-	end
-	
-	function events:LOADING_SCREEN_DISABLED(...) --should fire when the loading screen is enabled
-		--startTimeIGT = GetSessionTime();
-	end
+
 
 	--EVENTS THAT WILL START THE TIMER --
 	function events:ACTIONBAR_SHOWGRID(...) --This event fires any time an actionbar slot has been changed. This will not change is spell x is placed in the same slot as a spell x, but will change if spell x is removed
-		startTimer("ACTIONBAR_SHOWGRID")
+		startTimer("ACTIONBAR_UPDATE")
 
 	end
 
@@ -89,33 +71,44 @@ local WOW_HIGHEST_LEVEL = 110; --Initializes the highest level attainable by the
 	end
 	
 	function events:QUEST_ACCEPTED(...) --event will trigger when a quest is accepted
-		startTimer("QUEST_ACCEPTED")
+		startTimer("QUEST_STATUS_UPDATE")
 	end
 	
 	
 	----------------------
 	-- Actual Functions --
 	----------------------
-	function calculateRealTime(endTime, startTimeRT, ...) --Function used to calculate real time how long the run took
-		local timeTaken = difftime(endTime, startTimeRT) --creates a variable to hold the current time taken in seconds. Uses the difftime() funtion to subtract the 2nd (startTime) from the 1st(endTime) argument
-		print("Your Real Time: " .. floor(timeTaken/3600) .. " hours, " .. floor(timeTaken/60) .. " minutes, " .. timeTaken%60 .. " seconds.") --inline print function that will calculate hours, minutes, and seconds. Might be upgraded to milliseconds.
-	end
 
-	function calculateInGameTime(endTime, startTimeIGT, ...) --Function used to calculate in game time from when the player enters the world how long the run took
+	function calculateInGameTime(currentSplit, ...) --Function used to calculate in game time from when the player enters the world how long the run took
 
-		local timeTaken = difftime(endTime, startTimeIGT) + accumTimeIGT  --Function used to calculate real time how long the run took
-		print("Your In Game Time: " .. floor(timeTaken/3600) .. " hours, " .. floor(timeTaken/60) .. " minutes, " .. timeTaken%60 .. " seconds.") --inline print function that will calculate hours, minutes, and seconds. Might be upgraded to milliseconds.
+		--local timeTaken = difftime(endTime, startTimeIGT) + accumTimeIGT  --Function used to calculate real time how long the run took
+		print("Your In Game Time: " .. floor(currentSplit/3600) .. " hours, " .. floor(currentSplit/60) .. " minutes, " .. currentSplit%60 .. " seconds.") --inline print function that will calculate hours, minutes, and seconds. Might be upgraded to milliseconds.
 	
 	end
 	
 	function startTimer(argStartingCondition) --function that will handle deciding if the player can begin a run or not.
 		if playerLoaded == true then --if the player has acknowledged loading in to the game, and is ready...
-			if timerRunning == false then --and as long as the timer isn't already running
-				print("Timer has begun. Cause:", argStartingCondition) --let the user know the run is starting, and why the timer was started.
-				timerRunning = true; --set the timer running to true
-				startTimeRT = GetSessionTime(); --sets the RTstart to the currentSessionTime
-				startTimeIGT = GetSessionTime(); --sets the IGTstart to the currentSessionTime
+			if runStatus == false then
+			
+				if timerRunning == false then --and as long as the timer isn't already running
+					print("Timer has begun. Cause:", argStartingCondition) --let the user know the run is starting, and why the timer was started.
+					timerRunning = true; --set the timer running to true
+					startTimeIGT = GetSessionTime(); --sets the IGTstart to the currentSessionTime
+					runStatus = true;
+				end
+				
+
+				
 			end
+			
+			if runStatus == true then
+				if timerRunning == false then
+				timerRunning = true;
+				startTimeRT = GetSessionTime();
+			end
+
+		end
+		
 		else --if the player has not indecated that they are ready
 			print("Please reset the run. Reason for false start:", argStartingCondition) --any condition that would normally start the timer will fire, letting the user know they need to reset the run
 		end
@@ -126,54 +119,71 @@ local WOW_HIGHEST_LEVEL = 110; --Initializes the highest level attainable by the
 	-- Slash Commands --
 	--------------------
 	
-SLASH_SRGOAL1 = '/srGoal'; -- SLASH_PRINTTIME1 sets the PRINTTIME1 trigger word to '/printTime', variables must match SLASH_(NAME)(X) where NAME is the message grouping, and X is an incremental counter starting at 1
-	function SlashCmdList.SRGOAL(msg) -- sets the /timerGoal function, taking a message as the argument.
+	SLASH_SRGOAL1 = '/srGoal'; -- SLASH_PRINTTIME1 sets the PRINTTIME1 trigger word to '/printTime', variables must match SLASH_(NAME)(X) where NAME is the message grouping, and X is an incremental counter starting at 1
+		function SlashCmdList.SRGOAL(msg) -- sets the /timerGoal function, taking a message as the argument.
 
-		if tonumber(msg) then --if the slash command contains a message...
-			if tonumber(msg) > WOW_LOWEST_LEVEL and tonumber(msg) < WOW_HIGHEST_LEVEL then --if the message is a value within the acceptable values (stated at the start of this program)
-				goalLevel = msg --set the goal level to the msg sent
-				print("The current goal level is", goalLevel) --inform the user that the goal has changed
-			else --if there is a message that is NOT an integer between the accepted values
-				print("Please input a valid integer between 2 and 110 (Ex: 20). Current goal:", goalLevel) --let the user know the error, and show the current goal
+			if tonumber(msg) then --if the slash command contains a message...
+				if runStatus == false then
+					if tonumber(msg) >= WOW_LOWEST_LEVEL and tonumber(msg) <= WOW_HIGHEST_LEVEL then --if the message is a value within the acceptable values (stated at the start of this program)
+						goalLevel = msg --set the goal level to the msg sent
+						print("The current goal level is", goalLevel) --inform the user that the goal has changed
+					else --if there is a message that is NOT an integer between the accepted values
+						print("Please input a valid integer between 2 and 110 (Ex: 20). Current goal:", goalLevel) --let the user know the error, and show the current goal
+					end
+				end
+				
+				
+			end
+			if runStatus == true then
+				print("You are currently in a run. Please finish or reset before setting a new goal.")
+			end
+			
+			if not tonumber(msg) then --if no message is attached to the slash command...
+				if msg == "" then --if the message is empty...
+					print("The current goal level is", goalLevel) --just display the current goal level
+				else --if the user submits a message that can't be set to a number
+					print("Please input a valid integer between 2 and 110 (Ex: 20). Current goal:", goalLevel) --let the user know, and show the current goal level
+				end
 			end
 		end
+	
+	
+	SLASH_SRREADY1 = '/srReady'; -- SLASH_READY1 sets the READY1 trigger word to '/timerReady', must match input text exactly
+		function SlashCmdList.SRREADY(msg, editbox)
+			accumTimeIGT = 0;
+			currentSplit = 0;
+			playerLoaded = true; --shows that the player is loaded and ready to goal
+			print("Timer is ready. Timer will begin on first input.")
+		end
+
+	
+	SLASH_SRSPLIT1 = '/srSplit'; --SLASH_CURRENTIME1 sets the CURRENTTIME1 trigger to '/timerCurrentTime', and will display the current time, or split, of the run
+		function SlashCmdList.SRSPLIT()
 		
-		if not tonumber(msg) then --if no message is attached to the slash command...
-			if msg == "" then --if the message is empty...
-				print("The current goal level is", goalLevel) --just display the current goal level
-			else --if the user submits a message that can't be set to a number
-				print("Please input a valid integer between 2 and 110 (Ex: 20). Current goal:", goalLevel) --let the user know, and show the current goal level
+			if timerRunning == true and runStatus == true then --if the timer is currently running
+				endTime = GetSessionTime();
+				currentSplit = (difftime(endTime, startTimeIGT) + accumTimeIGT)
+				print("Your Real Time: " .. floor(currentSplit/3600) .. " hours, " .. floor(currentSplit/60) .. " minutes, " .. currentSplit%60 .. " seconds.")
+				
+			elseif timerRunning == false and runStatus == true then
+				print("Your Real Time: " .. floor(accumTimeIGT/3600) .. " hours, " .. floor(accumTimeIGT/60) .. " minutes, " .. accumTimeIGT%60 .. " seconds.")
+				print("Your In Game Time: " .. floor(accumTimeIGT/3600) .. " hours, " .. floor(accumTimeIGT/60) .. " minutes, " .. accumTimeIGT%60 .. " seconds.")
+		
+			else
+				print("The timer is not currently running. Please begin a run.") --placeholder line to show that the timer is not running
 			end
 		end
-		
-	end
+
 	
-SLASH_SRREADY1 = '/srReady'; -- SLASH_READY1 sets the READY1 trigger word to '/timerReady', must match input text exactly
-	function SlashCmdList.SRREADY(msg, editbox)
-		playerLoaded = true; --shows that the player is loaded and ready to goal
-		print("Timer is ready. Timer will begin on first input.")
-	end
-	
-SLASH_SRSPLIT1 = '/srSplit'; --SLASH_CURRENTIME1 sets the CURRENTTIME1 trigger to '/timerCurrentTime', and will display the current time, or split, of the run
-	function SlashCmdList.SRSPLIT() 
-		if timerRunning == true then --if the timer is currently running
-			local timeTaken = difftime(GetSessionTime(), startTimeIGT) + accumTimeIGT --set the current time taken to the difference between the current and start times, adding the accumulated times (For real time)
-			print("Your Real Time: " .. floor(timeTaken/3600) .. " hours, " .. floor(timeTaken/60) .. " minutes, " .. timeTaken%60 .. " seconds.")
-			print("Your In Game Time: " .. floor(timeTaken/3600) .. " hours, " .. floor(timeTaken/60) .. " minutes, " .. timeTaken%60 .. " seconds.")
-		--doesn't call the respective calculate functions because this isn't based on the final times, this is calculated as it is requested
-		else
-			print("The timer is not currently running. Please begin a run.") --placeholder line to show that the timer is not running
+	SLASH_SRSTOP1 = '/srStop';
+		function SlashCmdList.SRSTOP(msg, editbox)
+			print("Timer has been stopped.")
+					timerRunning = false; --set the timer running to false, since the timer is being stopped
+					playerLoaded = false; --sets the playerLoaded to false, allowing the user to restart should requirements be met
+					startTimeIGT = GetSessionTime(); --sets the current IGT timer to the currentSessionTime
+					accumTimeIGT = 0;
+					runStatus = false;
 		end
-	end
-	
-SLASH_SRSTOP1 = '/srStop';
-	function SlashCmdList.SRSTOP(msg, editbox)
-		print("Timer has been stopped.")
-				timerRunning = false; --set the timer running to false, since the timer is being stopped
-				playerLoaded = false; --sets the playerLoaded to false, allowing the user to restart should requirements be met
-				startTimeRT = GetSessionTime(); --sets the current RT timer to the currentSessionTime
-				startTimeIGT = GetSessionTime(); --sets the current IGT timer to the currentSessionTime
-	end
 	
 
 	--------------------
@@ -181,11 +191,10 @@ SLASH_SRSTOP1 = '/srStop';
 	--------------------
 	eventFrame:SetScript("OnEvent", function(self, event, ...) --Sets the scripts of all the event functions and how they will be handled
 	events[event](self, ...);
-	end);
+	end)
 	for k, v in pairs(events) do
-		eventFrame:RegisterEvent(k) --Registers each event as they are definied
-			
-end
+		eventFrame:RegisterEvent(k) --Registers each event as they are definied	
+	end
 
 
 
